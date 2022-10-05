5faa098aa65c31e468494d159e726b0eacb33e91
mark4335 <83931147+mark4335@users.noreply.github.com>
2022-10-04 19:02:51 -0600

    Added PHP

---
+++ Project 3/index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<?php echo "<body style='background-color:black'>" ?>
+<?php $textcolor = $_POST["color"]; ?>
+<?php echo "<text style='color:$textcolor'>" ?>
+<?php echo $_POST["name"]; ?><br>
+
+
+</body>
+</html> 
